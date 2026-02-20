a26bdad3a09e6c0f20f649dd02b5c86d16607f09
Pankaj Singh <pankajsingh1397@gmail.com>
2026-02-20 10:37:12 +0530

    chore: update UI version

--- install.sh
+++ install.sh
@@ -197,7 +197,7 @@ distro_factory() {
 # ****************************************
 
 add_frontend() {
-    VERSION="v1.1.0-alpha.1"
+    VERSION="v1.1.0"
     log "Pulling frontend version ${VERSION}"
     mkdir -p $script_dir/frontend
     cd $script_dir/frontend
